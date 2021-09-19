52a4836e2997e23155b68f653f8975329a9c7406
Pingweiyi <90896464+Pingweiyi@users.noreply.github.com>
2021-09-20 05:09:07 +0800

    Add files via upload

---
+++ task.js
@@ -0,0 +1,42 @@
+var task = new Vue({
+  el: '#task',
+  data: {
+  Hi:"Hello!",
+  name: "Pingweiyi Yu.",
+  inf2:"I am currently studying at Deakin University and my major is Network and Cloud computing.",
+  inf:"I am a online student from China. ",
+  image:"",
+  }
+});
+
+var wtg = new Vue({
+  el: '#wth',
+  data: {
+  weather:true ,
+   },
+  });
+
+
+  
+var aaa = new Vue({
+    el: '#aaa',
+    data: {
+      name: 'User'
+    },
+
+    methods: {
+      hi: function () {
+        
+        alert('Hello ' + this.name + '!')
+
+      }
+    }
+  })
+  
+var bbb = new Vue({
+    el: '#bbb',
+    data: {
+      msg: "Please enter your user ID"
+    }
+  })
+  
---
+++ week5.js
@@ -0,0 +1,18 @@
+var msg = new Vue({
+  el: '#msg',
+  data: {
+  name: "Hello user,",
+  welcome:"welcome to the web page!",
+  inf:" This website can provide you with more detailed game information and information, wish you have a good time!",
+	
+  }
+});
+
+
+
+var announcement = new Vue({
+  el: '#announcement',
+  data: {
+  updated: true,
+   },
+  });
